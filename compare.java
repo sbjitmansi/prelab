4321b625705f1b1f589ee8252016257ae68188d4
MANSI SAWARKAR <120084878+sbjitmansi@users.noreply.github.com>
2023-08-31 12:15:02 +0530

    Create compare.java

---
+++ compare.java
@@ -0,0 +1,14 @@
+public class Trial
+{
+public static void main(String []args)
+{
+int a=20;
+double b=4;
+if (a>b){
+System.out.println(a>b);
+System.out.println("a>b"); }
+else{
+System.out.println(a<b);
+System.out.println("a<b"); }
+}
+}
